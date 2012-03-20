141306936bad359d41721c184052bdb561e5f525
micahasmith <micah.a.smith@gmail.com>
2012-03-20 00:36:25 -0300

    Update location-analytics.js

--- location-analytics.js
+++ location-analytics.js
@@ -1,7 +1,7 @@
 var GAH = GAH || {};
 GAH.locationAnalytics=(function(){
 	var init=function(gaq, onNoSupportFunc){
-		var cookieSetting="HGLA=1"
+		var cookieSetting="HGLA2=1"
 			doesntSupportFunc=onNoSupportFunc || function(){ document.cookie=cookieSetting; }
 			,shouldTest=true
 			,i=0
@@ -11,7 +11,7 @@ GAH.locationAnalytics=(function(){
 			if(document.cookie){
 				len=document.cookie.length;
 				for(;i<len;i+=1){
-					if(document.cookie[i].indexOf("HLA2=")!==-1){
+					if(document.cookie[i].indexOf("HGLA2=")!==-1){
 						shouldTest=false;
 					}
 				}
@@ -20,13 +20,13 @@ GAH.locationAnalytics=(function(){
 				if(navigator.geolocation){
 					navigator.geolocation.getCurrentPosition(function(position){
 						geo=position.coords.longitude.toString()+","+position.coords.latitude;
-						gaq.push(['_setCustomVar',1,"Visitor_GeoLocation",geo,1]);
+						gaq.push(['_setCustomVar',1,"Visitor_GeoLocation",geo,2]);
 						document.cookie=cookieSetting;
 					},function(){
-						doesntSupportFunc();
+						gaq.push(['_setCustomVar',1,"Visitor_GeoLocation","error",2]);
 					});
 				} else {
-					doesntSupportFunc();
+					gaq.push(['_setCustomVar',1,"Visitor_GeoLocation","denied",2]);
 				}
 			}
 		}
