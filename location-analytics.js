7f18bc6c6694516187919d35c1374c2d89adfcfc
micahasmith <micah.a.smith@gmail.com>
2012-04-27 10:54:53 -0300

    removed dependency from google and made it callback oriented instead.

--- location-analytics.js
+++ location-analytics.js
@@ -1,13 +1,13 @@
 var GAH = GAH || {};
 GAH.locationAnalytics=(function(){
-	var init=function(gaq, onNoSupportFunc){
+	var init=function(onNoSupportFunc,cb){
 		var cookieSetting="HGLA3=1"
 			doesntSupportFunc=onNoSupportFunc || function(){ document.cookie=cookieSetting; }
 			,shouldTest=true
 			,i=0
 			,len=0
 			,geo="";
-		if(typeof gaq!=='undefined'){
+		
 			if(document.cookie){
 				len=document.cookie.length;
 				for(;i<len;i+=1){
@@ -20,16 +20,16 @@ GAH.locationAnalytics=(function(){
 				if(navigator.geolocation){
 					navigator.geolocation.getCurrentPosition(function(position){
 						geo=position.coords.longitude.toString()+","+position.coords.latitude;
-						gaq.push(['_setCustomVar',5,"Visitor_GeoLocation",geo,1]);
+						cb('success',geo);
 						document.cookie=cookieSetting;
 					},function(){
-						gaq.push(['_setCustomVar',5,"Visitor_GeoLocation","error",1]);
+						cb('error',geo);
 					});
 				} else {
-					gaq.push(['_setCustomVar',5,"Visitor_GeoLocation","denied",1]);
+					cb('denied');
 				}
 			}
-		}
+		
 	}
 	return {init:init};
 })();
